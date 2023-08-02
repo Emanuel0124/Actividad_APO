24398d96f85a835896a618829c84f46dc72d2d55
Emauel0124 <emanuelbedoya0124@gmail.com>
2023-08-02 18:18:57 -0500

    Se completa el punto 2

---
+++ Ejercicios/Punto2.py
@@ -0,0 +1,8 @@
+def Area_circulo ():
+    Radio = float(input("Ingrese el radi del circulo: "))
+
+    Area=Radio*Radio*3.14
+
+    mensaje=f"El area del circulo con radio {Radio} es: {Area}"
+
+    return mensaje
--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-from Ejercicios import Punto1
+from Ejercicios import Punto1 as Pt1
+from Ejercicios import Punto2 as Pt2
 
-Punto1.preguntar_nombre_y_edad()
+
+
+while True:
+    print("Opcion 1: Punto1 ")
+    print("Opcion 2: Punto2 ")
+    print("Opcion 3: Punto3 ")
+    print("Opcion 4: Punto4 ")
+    print("Opcion 5: Punto5 ")
+    print("Opcion 6: Punto6 ")
+    print("Opcion 7: Punto7 ")
+    print("Opcion 8: Punto8 ")
+    print("Opcion 9: Punto9 ")
+    print("Opcion 10: Punto10 ")
+    print("Opcion 11: Punto11 ")
+    print("Opcion 12: Punto12 ")
+    print("Opcion 13: Punto13 ")
+    print("Opcion 14: Punto14 ")
+    print("Opcion 15: Punto15 ")
+    print("Opcion 16: Salir ")
+    opcion=int(input("Ingrese la opcion que desea ejecutar: "))
+    
+    if (opcion==1):
+        print(Pt1.preguntar_nombre_y_edad()) 
+        opcion=int(input("Ingrese cualquier numero para volver al menú "))
+    elif (opcion==2):
+        print(Pt2.Area_circulo())
+        opcion=int(input("Ingrese cualquier numero para volver al menú "))
+
+    elif (opcion==3):
+        print(())
+        opcion=int(input("Ingrese cualquier numero para volver al menú "))
 
